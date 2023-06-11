4bf4e10ef1c05de73d36e92d1b156c76f155bdc2
shaneyoung91 <shaneadam.young@gmail.com>
2023-06-11 14:16:10 -0700

    wireframed the UI

---
+++ main.js
@@ -0,0 +1,27 @@
+/*----- constants -----*/
+
+
+/*----- state variables -----*/
+
+// turn: 1/-1 (represents player "X" and player "O")
+// board: 2D array, 3x3 grid
+	// null => no player
+	// 1/-1 => player at that cell
+
+	//  [ null, null, null
+	//    null, null, null
+	//    null, null, null ]
+
+// winner:
+	// null => no winner
+	// 1/-1 => winner
+	// 'T' => tie
+
+	
+/*----- cached elements  -----*/
+
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
