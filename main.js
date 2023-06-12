c810548fe8a5b2d28e4656e2c8eb063781804daf
shaneyoung91 <shaneadam.young@gmail.com>
2023-06-11 21:27:26 -0700

    updated handleclick event

--- main.js
+++ main.js
@@ -23,36 +23,46 @@ let board; // 2D array, 3x3 grid
 let turn; // represented by 1 or -1
 let winner; // null = no winner; 1 or -1 = winner; "T" = tie game;
 
-/*----- cached elements  -----*/
+/*----- cached (save/remember) elements  -----*/
 const messageElement = document.querySelector('h1');
-
-
+const resetGameBtn = document.querySelector('button');
+const squareEls = [...document.querySelectorAll('#board > div')];
 
 /*----- event listeners -----*/
-
+document.getElementById('board').addEventListener('click', handleClick);
 
 
 /*----- functions -----*/
 initialize();
 
-	// Initialize all state, then call render()
 function initialize() {
-	// To visualize the board mapping to the DOM,
-	// rotate the board array 90 degrees counter-clockwise
 	board = [
-		[0, 0, 0], // column 0
-		[0, 0, 0], // column 1
-		[0, 0, 0],  // column 2
-	];
+		[0, 0, 0],
+		[0, 0, 0],
+		[0, 0, 0]
+	]
 	turn = 1;
 	winner = null;
 	render();
 }
 
+function handleClick(evt) {
+	const squareId = evt.target.id
+	const colIdx = parseInt(squareId.charAt(1));
+	const rowIdx = parseInt(squareId.charAt(3));
+	if (board[colIdx][rowIdx] === 0 && !winner) {
+		board[colIdx][rowIdx] = turn;
+		turn *= -1;
+		winner = getWinner(colIdx, rowIdx)
+		render();
+	}
+};
+
+
 function render() {
 	renderBoard();
 	renderMessage();
-	renderControls(); // Hide/show UI elements (reset game button)
+	renderControls();
 }
 
 function renderBoard() {
@@ -67,16 +77,17 @@ function renderBoard() {
 
 function renderMessage(){
 	if (winner === 'T') {
-		messageElement.innerText = "It's A Tie!"
+		messageElement.innertext = "It's A Tie!";
 	} else if (winner) {
-		messageElement.innerHTML = `<span style="color: ${COLORS[winner]}">${PLAYERS[winner]} Wins!`;
+		messageElement.innerHTML = `<span style="color: ${COLORS[winner]}">${PLAYERS[winner].toUpperCase()}</span> Wins!`;
 	} else {
-		messageElement.innerHTML = `<span style="color: ${COLORS[turn]}">${PLAYERS[turn]}</span>'s Turn'`;
+		messageElement.innerHTML = `<span style="color: ${COLORS[turn]}">${PLAYERS[turn].toUpperCase()}</span>'s Turn`;
 	}
 };
 
 function renderControls() {
-
-}
-
+	// use ternary (conditional) expression when you want 1 of 2 values returned (truthy and falsy)
+	// <cond expression> ? <truthy exp> : <falsy exp>
+	resetGameBtn.style.visibility = winner ? 'visible' : 'hidden';
+};
 
